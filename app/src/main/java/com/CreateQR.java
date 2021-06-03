6f84e7c4c8d14557b82d9c59bda638595905dead
myles <myles_uy@dlsu.edu.ph>
2021-06-03 15:02:44 +0800

    Updated codes

--- app/src/main/java/com/CreateQR.java
+++ app/src/main/java/com/CreateQR.java
@@ -42,7 +42,7 @@ public class CreateQR extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_create_qr);
 
         FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
 
